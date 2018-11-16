fd6d924c02261e85d832b29f2d85c93a91604b20
Kevin Hoang <find.kevinhoang@gmail.com>
2018-11-16 15:23:51 -0500

    return gift card amount on successful redemption

--- app/controllers/spree/api/gift_cards_controller.rb
+++ app/controllers/spree/api/gift_cards_controller.rb
@@ -1,5 +1,4 @@
 class Spree::Api::GiftCardsController < Spree::Api::BaseController
-
   def redeem
     redemption_code = Spree::RedemptionCodeGenerator.format_redemption_code_for_lookup(params[:redemption_code] || "")
     @gift_card = Spree::VirtualGiftCard.active_by_redemption_code(redemption_code)
@@ -7,7 +6,7 @@ class Spree::Api::GiftCardsController < Spree::Api::BaseController
     if !@gift_card
       render status: :not_found, json: redeem_fail_response
     elsif @gift_card.redeem(@current_api_user)
-      render status: :created, json: {}
+      render status: :created, json: { amount: @gift_card.formatted_amount }
     else
       render status: 422, json: redeem_fail_response
     end
@@ -16,8 +15,8 @@ class Spree::Api::GiftCardsController < Spree::Api::BaseController
   private
 
   def redeem_fail_response
-   {
+    {
       error_message: "#{Spree.t('admin.gift_cards.errors.not_found')}. #{Spree.t('admin.gift_cards.errors.please_try_again')}"
-   }
- end
+    }
+  end
 end
