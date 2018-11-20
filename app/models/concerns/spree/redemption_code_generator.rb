e303b4d570a2ab29cb655a5d64c822f0118345d6
Kevin Hoang <find.kevinhoang@gmail.com>
2018-11-20 13:47:09 -0500

    just remove method

--- app/models/concerns/spree/redemption_code_generator.rb
+++ app/models/concerns/spree/redemption_code_generator.rb
@@ -1,16 +1,10 @@
 module Spree::RedemptionCodeGenerator
   def self.generate_redemption_code
-    chars = [('A'..'Z'), ('0'..'9')].map(&:to_a).flatten - omitted_chars
+    chars = [('A'..'Z'), ('0'..'9')].map(&:to_a).flatten - %w[O L I B 0 1 8]
     16.times.map { chars[rand(chars.count)] }.join
   end
 
   def self.format_redemption_code_for_lookup(redemption_code)
     redemption_code.delete('-').upcase
-  end
-
-  private
-
-  def self.omitted_chars
-    %w[O L I B 0 1 8]
   end
 end
